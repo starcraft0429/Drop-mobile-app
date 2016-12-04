41d75d53257e0444bd218f86b7454735440fdc28
starcraft0429 <starcraft0429@hotmail.com>
2016-12-05 02:45:54 +0300

    backup

--- components/mainView.js
+++ components/mainView.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
+import { 
+          StyleSheet, 
+          Text, 
+          View, 
+          Button,
+          Image,
+          StatusBar, 
+          Dimensions 
+        } from 'react-native';
 
-class mainView extends React.Component {
+class mainViewCompo extends React.Component {
   constructor(props) {
     super(props)
     
@@ -10,13 +18,25 @@ class mainView extends React.Component {
 
   render() {
     return (
-    	<View>
+    	<View style={{flex: 1}}>
         <StatusBar
           backgroundColor="#ffffff"
           barStyle="dark-content"
         />
-        <Text>mainView</Text>
-        
+        <View style={styles.navbar}>
+          <Text style={styles.navbarText}>One swipe to order!</Text>
+        </View>
+        <View style={styles.mainView}>
+          <View style={styles.dropView}>
+            <Image source={require('../images/main_slider.png')} style={styles.bgImg}>
+              <Text style={styles.welcome}>
+                Welcome to React Native!gfiyfgygyujfgysdfasdfasdfsadfdsa
+              </Text>
+            </Image>
+          </View>
+          <View style={styles.btnView}>
+          </View>
+        </View>
       </View>
       
     );
@@ -25,4 +45,45 @@ class mainView extends React.Component {
 }
 
 
-export default mainView
+export default mainViewCompo
+
+var DeviceWidth = Dimensions.get('window').width;
+var DeviceHeight = Dimensions.get('window').height;
+
+var styles = StyleSheet.create({
+  navbar:{
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#dddddd',
+  },
+  navbarText:{
+    fontSize: 20,
+    fontFamily: 'Lato-Medium',
+    color: '#000000'
+  },
+  mainView:{
+    backgroundColor: '#ededed',
+    flex: 1,
+  },
+  dropView:{
+    flex: .55,
+    padding: 10,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    
+  },
+  btnView:{
+    backgroundColor: 'blue',
+    flex: .45,
+
+  },
+  bgImg:{
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    backgroundColor: 'aqua',
+    resizeMode: 'contain',
+  }
+});
