e5d4019e2e8d8604ba805ee308315182387a0b71
starcraft0429 <starcraft0429@hotmail.com>
2016-12-05 15:37:17 +0300

    menu basic implement

--- components/mainView.js
+++ components/mainView.js
@@ -6,9 +6,15 @@ import {
           Button,
           Image,
           StatusBar, 
-          Dimensions 
+          Dimensions,
+          TouchableHighlight
         } from 'react-native';
-
+import Menu, {
+                MenuContext,
+                MenuOptions,
+                MenuOption,
+                MenuTrigger
+              } from 'react-native-popup-menu';
 class mainViewCompo extends React.Component {
   constructor(props) {
     super(props)
@@ -23,44 +29,63 @@ class mainViewCompo extends React.Component {
           backgroundColor="#ffffff"
           barStyle="dark-content"
         />
-        <View style={styles.navbar}>
-          <Text style={styles.navbarText}>One swipe to order!</Text>
-        </View>
-        <View style={styles.mainView}>
-          <View style={styles.dropView}>
-            <Image source={require('../images/main_slider.png')} style={styles.bgImg}>
-              <Image source={require('../images/main_slide_button_letters.png')} style={styles.circleImg}>
-              
+        <MenuContext>
+          <View style={styles.navbar}>
+            <View style={styles.menuView}></View>
+            <View style={styles.navbarTextView}>
+              <Text style={styles.navbarText}>One swipe to order!</Text>
+            </View>
+            <View style={styles.menuView}>
+              <Menu onSelect={value => alert(`Selected number: ${value}`)}>
+                <MenuTrigger>
+                  <Text style={styles.menuText}>● ● ●</Text>
+                </MenuTrigger>
+                <MenuOptions>
+                  <MenuOption value={1} text='One' />
+                  <MenuOption value={2}>
+                    <Text style={{color: 'red'}}>Two</Text>
+                  </MenuOption>
+                  <MenuOption value={3} disabled={true} text='Three' />
+                </MenuOptions>
+              </Menu>
+            </View>
+          </View>
+          <View style={styles.mainView}>
+            <View style={styles.dropView}>
+              <Image source={require('../images/main_slider.png')} style={styles.bgImg}>
+                <Image source={require('../images/main_slide_button_letters.png')} style={styles.circleImg}>
+                
+                </Image>
               </Image>
-            </Image>
+            </View>
+            <View style={styles.btnView}>
+              <View style={styles.btnRow}>
+                <Image style={styles.btnImg}
+                  source={require('../images/main_time_icon.png')}
+                />
+                <Text style={styles.btnText}>7:00pm - 9:00pm Today</Text>
+              </View>
+              <View style={styles.btnRow}>
+                <Image style={styles.btnImg}
+                  source={require('../images/main_delivery_icon.png')}
+                />
+                <Text style={styles.btnText}>Duong Vung</Text>
+              </View>
+              <View style={styles.btnRow}>
+                <Image style={styles.btnImg}
+                  source={require('../images/main_location_icon.png')}
+                />
+                <Text style={styles.btnText}>Home</Text>
+              </View>
+              <View style={styles.btnRow}>
+                <Image style={styles.btnImg}
+                  source={require('../images/main_shoppingcart_icon.png')}
+                />
+                <Text style={styles.btnText}>150,000d for 13 items</Text>
+              </View>
+            </View>
           </View>
-          <View style={styles.btnView}>
-            <View style={styles.btnRow}>
-              <Image style={styles.btnImg}
-                source={require('../images/main_time_icon.png')}
-              />
-              <Text style={styles.btnText}>7:00pm - 9:00pm Today</Text>
-            </View>
-            <View style={styles.btnRow}>
-              <Image style={styles.btnImg}
-                source={require('../images/main_delivery_icon.png')}
-              />
-              <Text style={styles.btnText}>Duong Vung</Text>
-            </View>
-            <View style={styles.btnRow}>
-              <Image style={styles.btnImg}
-                source={require('../images/main_location_icon.png')}
-              />
-              <Text style={styles.btnText}>Home</Text>
-            </View>
-            <View style={styles.btnRow}>
-              <Image style={styles.btnImg}
-                source={require('../images/main_shoppingcart_icon.png')}
-              />
-              <Text style={styles.btnText}>150,000d for 13 items</Text>
-            </View>
-          </View>
-        </View>
+        </MenuContext>
       </View>
       
     );
@@ -76,16 +101,29 @@ var DeviceHeight = Dimensions.get('window').height;
 
 var styles = StyleSheet.create({
   navbar:{
+    flexDirection: 'row',
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#dddddd',
+  },
+  navbarTextView:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   navbarText:{
     fontSize: 20,
     fontFamily: 'Lato-Medium',
-    color: '#000000'
+    color: '#000000',
+  },
+  menuView:{
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+  },
+  menuText:{
+    color: '#333',
+    fontSize: 15,
   },
   mainView:{
     backgroundColor: '#ededed',
