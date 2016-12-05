ca80cab25618db50a8c3907c4644d596ce4a9667
starcraft0429 <starcraft0429@hotmail.com>
2016-12-05 12:18:57 +0300

    mainview update

--- components/mainView.js
+++ components/mainView.js
@@ -29,12 +29,36 @@ class mainViewCompo extends React.Component {
         <View style={styles.mainView}>
           <View style={styles.dropView}>
             <Image source={require('../images/main_slider.png')} style={styles.bgImg}>
-              <Text style={styles.welcome}>
-                Welcome to React Native!gfiyfgygyujfgysdfasdfasdfsadfdsa
-              </Text>
+              <Image source={require('../images/main_slide_button_letters.png')} style={styles.circleImg}>
+              
+              </Image>
             </Image>
           </View>
           <View style={styles.btnView}>
+            <View style={styles.btnRow}>
+              <Image style={styles.btnImg}
+                source={require('../images/main_time_icon.png')}
+              />
+              <Text style={styles.btnText}>7:00pm - 9:00pm Today</Text>
+            </View>
+            <View style={styles.btnRow}>
+              <Image style={styles.btnImg}
+                source={require('../images/main_delivery_icon.png')}
+              />
+              <Text style={styles.btnText}>Duong Vung</Text>
+            </View>
+            <View style={styles.btnRow}>
+              <Image style={styles.btnImg}
+                source={require('../images/main_location_icon.png')}
+              />
+              <Text style={styles.btnText}>Home</Text>
+            </View>
+            <View style={styles.btnRow}>
+              <Image style={styles.btnImg}
+                source={require('../images/main_shoppingcart_icon.png')}
+              />
+              <Text style={styles.btnText}>150,000d for 13 items</Text>
+            </View>
           </View>
         </View>
       </View>
@@ -66,24 +90,44 @@ var styles = StyleSheet.create({
   mainView:{
     backgroundColor: '#ededed',
     flex: 1,
+    padding: 20,
   },
   dropView:{
     flex: .55,
-    padding: 10,
     alignSelf: 'stretch',
     alignItems: 'center',
     
-  },
-  btnView:{
-    backgroundColor: 'blue',
-    flex: .45,
-
   },
   bgImg:{
     flex: 1,
     width: undefined,
     height: undefined,
-    backgroundColor: 'aqua',
     resizeMode: 'contain',
+  },
+  circleImg:{
+    width: 220,
+    height: 220,
+  },
+  btnView:{
+    flex: .45,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 10,
+  },
+  btnRow:{
+    height: 50,
+    width: 350,
+    flexDirection: 'row',
+    alignItems: 'center',
+    
+  },
+  btnImg:{
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  btnText:{
+    fontSize: 18,
+    fontFamily: 'Lato-Regular',
   }
 });
